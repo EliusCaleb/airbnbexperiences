3d1c9fc83abc587dc3410e33edca8ffd7d9a755f
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-12-18 18:18:59 +0300

    added links for images

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: "katie-zaferes.png",
+        coverImg: "https://images.unsplash.com/photo-1525025486157-6b19617002b5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -17,7 +17,7 @@
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: "wedding-photography.png",
+        coverImg: "https://images.unsplash.com/photo-1595467959776-8acfb0dc313f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -30,7 +30,7 @@
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "mountain-bike.png",
+        coverImg: "https://images.unsplash.com/photo-1572111504021-40abd3479ddb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         stats: {
             rating: 4.8,
             reviewCount: 2
