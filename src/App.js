465bd55e43584b6c7cc355b2951fb50fb5c22f14
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-12-18 18:19:15 +0300

    added card components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import Navbar from './components/Navbar';
-import './App.css';
-import { Hero } from './components/Hero';
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { Hero } from "./components/Hero";
+import data from "./data";
+import Card from "./components/Card";
 
 function App() {
+  //console.log(data)
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
   return (
     <div className="App">
-        <Navbar/>
-        <Hero/>
-      
+      <Navbar />
+      <Hero />
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
